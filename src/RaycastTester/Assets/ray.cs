502711259030b7a7519a54dca86bed282fa35316
Daniel Inversini <inved1@bfh.ch>
2015-10-20 17:50:53 +0200

    raycast Tester

---
+++ src/RaycastTester/Assets/ray.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ray : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit o;
+        if(Physics.Raycast(r, out o))
+        {
+            Debug.Log(string.Format("info hit: {0}", GetComponent<Camera>().WorldToScreenPoint(o.point)));
+        }
+        
+
+	
+	}
+}
